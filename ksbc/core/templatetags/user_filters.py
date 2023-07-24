205920cdb4da1875eb793b4e4dc743658c6711e1
Margulan Zhanay <mcmrkr@gmail.com>
2023-07-24 15:30:23 +0800

    Delete user filter

--- ksbc/core/templatetags/user_filters.py
+++ ksbc/core/templatetags/user_filters.py
@@ -6,8 +6,3 @@ register = template.Library()
 @register.filter
 def addclass(field, css):
     return field.as_widget(attrs={'class': css})
-
-
-@register.filter
-def add_active_class(url, current_url):
-    return 'active' if url == current_url else ''
